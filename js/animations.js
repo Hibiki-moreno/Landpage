5f1338236588f236e533ad25aedbc1bac35cf40a
Hibiki <hibikicarlo@gmail.com>
2025-01-29 02:18:09 -0600

    menu

---
+++ js/animations.js
@@ -0,0 +1,3 @@
+gsap.to("#title", { opacity: 1, y: -10, duration: 1 });
+gsap.to("#description", { opacity: 1, y: -10, duration: 1, delay: 0.5 });
+gsap.to("#buttonContainer", { opacity: 1, scale: 1, duration: 0.8, delay: 1 });
